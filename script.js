732137f561fcdcb3ae90cc65754d5677235d0d2a
Nithyasree8 <113105796+Nithyasree8@users.noreply.github.com>
2024-09-15 10:34:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	if (s.length === 0) {
+        return '';
+    }
+    const index = s.indexOf(' ');
+    if (index === -1) {
+        return s;
+    }
+
+    return s.substring(0, index);
 }
-
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
